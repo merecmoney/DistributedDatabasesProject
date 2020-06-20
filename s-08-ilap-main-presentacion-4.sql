6f731e7317afd55ee87ff338179f1b4d51fcdb43
Marco Antonio Moreno <marcomorenoag@outlook.com>
2020-06-20 01:50:47 -0500

    set serveroutput on in every connection in presentation 4

--- s-08-ilap-main-presentacion-4.sql
+++ s-08-ilap-main-presentacion-4.sql
@@ -5,29 +5,32 @@
 clear screen
 whenever sqlerror exit rollback;
 
-set serveroutput on
 prompt =================================================
 prompt Ejecutando validador presentacion 4 en cahabdd_s1
 prompt =================================================
 connect ilap_bdd/ilap_bdd@cahabdd_s1
+set serveroutput on
 @s-08-ilap-presentacion-4.plb
 
 prompt =================================================
 prompt Ejecutando validador presentacion 4 en cahabdd_s2
 prompt =================================================
 connect ilap_bdd/ilap_bdd@cahabdd_s2
+set serveroutput on
 @s-08-ilap-presentacion-4.plb
 
 prompt =================================================
 prompt Ejecutando validador presentacion 4 en mamgbd_s1
 prompt =================================================
 connect ilap_bdd/ilap_bdd@mamgbd_s1
+set serveroutput on
 @s-08-ilap-presentacion-4.plb
 
 prompt =================================================
 prompt Ejecutando validador presentacion 4 en mamgbd_s2
 prompt =================================================
 connect ilap_bdd/ilap_bdd@mamgbd_s2
+set serveroutput on
 @s-08-ilap-presentacion-4.plb
 
 Prompt Listo!
