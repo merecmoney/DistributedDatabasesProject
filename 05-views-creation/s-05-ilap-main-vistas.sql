3563458ecbf513d20a27293d064951e3c9c69267
Marco Antonio Moreno <marcomorenoag@outlook.com>
2020-06-19 22:13:53 -0500

    Updated subpath references to main scripts 05 and 06

--- 05-views-creation/s-05-ilap-main-vistas.sql
+++ 05-views-creation/s-05-ilap-main-vistas.sql
@@ -10,60 +10,60 @@ prompt Creando vistas para cahabdd_s1
 prompt ===========================================
 connect ilap_bdd/ilap_bdd@cahabdd_s1
 prompt creando vistas que no requieren manejo de BLOBs
-@s-05-ilap-vistas.sql
+@05-views-creation/s-05-ilap-vistas.sql
 prompt creando vistas de replicación
-@s-05-ilap-netmax-vistas
+@05-views-creation/s-05-ilap-netmax-vistas
 prompt creando tablas temporales
-@s-05-ilap-tablas-temporales.sql
+@05-views-creation/s-05-ilap-tablas-temporales.sql
 prompt creando objetos para manejo de BLOBs
-@s-05-ilap-funciones-blob.sql
+@05-views-creation/s-05-ilap-funciones-blob.sql
 prompt creando vistas con soporte para BLOBs
-@s-05-ilap-cah-s1-vistas-blob.sql
+@05-views-creation/s-05-ilap-cah-s1-vistas-blob.sql
 
 prompt ===========================================
 prompt Creando vistas para cahabdd_s2
 prompt ===========================================
 connect ilap_bdd/ilap_bdd@cahabdd_s2
 prompt creando vistas que no requieren manejo de BLOBs
-@s-05-ilap-vistas.sql
+@05-views-creation/s-05-ilap-vistas.sql
 prompt creando vistas de replicación
-@s-05-ilap-netmax-vistas
+@05-views-creation/s-05-ilap-netmax-vistas
 prompt creando tablas temporales
-@s-05-ilap-tablas-temporales.sql
+@05-views-creation/s-05-ilap-tablas-temporales.sql
 prompt creando objetos para manejo de BLOBs
-@s-05-ilap-funciones-blob.sql
+@05-views-creation/s-05-ilap-funciones-blob.sql
 prompt creando vistas con soporte para BLOBs
-@s-05-ilap-cah-s2-vistas-blob.sql
+@05-views-creation/s-05-ilap-cah-s2-vistas-blob.sql
 
 prompt ===========================================
 prompt Creando vistas para mamgbd_s1
 prompt ===========================================
 connect ilap_bdd/ilap_bdd@mamgbd_s1
 prompt creando vistas que no requieren manejo de BLOBs
-@s-05-ilap-vistas.sql
+@05-views-creation/s-05-ilap-vistas.sql
 prompt creando vistas de replicación
-@s-05-ilap-netmax-vistas
+@05-views-creation/s-05-ilap-netmax-vistas
 prompt creando tablas temporales
-@s-05-ilap-tablas-temporales.sql
+@05-views-creation/s-05-ilap-tablas-temporales.sql
 prompt creando objetos para manejo de BLOBs
-@s-05-ilap-funciones-blob.sql
+@05-views-creation/s-05-ilap-funciones-blob.sql
 prompt creando vistas con soporte para BLOBs
-@s-05-ilap-mmg-s1-vistas-blob.sql
+@05-views-creation/s-05-ilap-mmg-s1-vistas-blob.sql
 
 prompt ===========================================
 prompt Creando vistas para mamgbd_s2
 prompt ===========================================
 connect ilap_bdd/ilap_bdd@mamgbd_s2
 prompt creando vistas que no requieren manejo de BLOBs
-@s-05-ilap-vistas.sql
+@05-views-creation/s-05-ilap-vistas.sql
 prompt creando vistas de replicación
-@s-05-ilap-netmax-vistas
+@05-views-creation/s-05-ilap-netmax-vistas
 prompt creando tablas temporales
-@s-05-ilap-tablas-temporales.sql
+@05-views-creation/s-05-ilap-tablas-temporales.sql
 prompt creando objetos para manejo de BLOBs
-@s-05-ilap-funciones-blob.sql
+@05-views-creation/s-05-ilap-funciones-blob.sql
 prompt creando vistas con soporte para BLOBs
-@s-05-ilap-mmg-s2-vistas-blob.sql
+@05-views-creation/s-05-ilap-mmg-s2-vistas-blob.sql
 
 --completar
 prompt Listo!
--- 06-triggers/s-06-ilap-main-triggers.sql
+++ 06-triggers/s-06-ilap-main-triggers.sql
@@ -9,64 +9,64 @@ Prompt ==============================
 Prompt Creando triggers en cahabdd_s1
 Prompt ==============================
 connect ilap_bdd/ilap_bdd@cahabdd_s1
-@s-06-ilap-sucursal-trigger.sql
-@s-06-ilap-cah-s1-sucursal-taller-trigger.sql
-@s-06-ilap-cah-s1-sucursal-venta-trigger.sql
-@s-06-ilap-laptop-trigger.sql
-@s-06-ilap-laptop-inventario-trigger.sql
-@s-06-ilap-historico-status-laptop-trigger.sql
-@s-06-ilap-cah-s1-servicio-laptop-trigger.sql
-@s-06-netmax-tipo-almacenamiento-trigger.sql
-@s-06-netmax-tipo-monitor-trigger.sql
-@s-06-netmax-tipo-procesador-trigger.sql
-@s-06-netmax-tipo-tarjeta-video-trigger.sql
+@06-triggers/s-06-ilap-sucursal-trigger.sql
+@06-triggers/s-06-ilap-cah-s1-sucursal-taller-trigger.sql
+@06-triggers/s-06-ilap-cah-s1-sucursal-venta-trigger.sql
+@06-triggers/s-06-ilap-laptop-trigger.sql
+@06-triggers/s-06-ilap-laptop-inventario-trigger.sql
+@06-triggers/s-06-ilap-historico-status-laptop-trigger.sql
+@06-triggers/s-06-ilap-cah-s1-servicio-laptop-trigger.sql
+@06-triggers/s-06-netmax-tipo-almacenamiento-trigger.sql
+@06-triggers/s-06-netmax-tipo-monitor-trigger.sql
+@06-triggers/s-06-netmax-tipo-procesador-trigger.sql
+@06-triggers/s-06-netmax-tipo-tarjeta-video-trigger.sql
 
 Prompt ==============================
 Prompt Creando triggers en cahabdd_s2
 Prompt ==============================
 connect ilap_bdd/ilap_bdd@cahabdd_s2
-@s-06-ilap-sucursal-trigger.sql
-@s-06-ilap-cah-s2-sucursal-taller-trigger.sql
-@s-06-ilap-cah-s2-sucursal-venta-trigger.sql
-@s-06-ilap-laptop-trigger.sql
-@s-06-ilap-laptop-inventario-trigger.sql
-@s-06-ilap-historico-status-laptop-trigger.sql
-@s-06-ilap-cah-s2-servicio-laptop-trigger.sql
-@s-06-netmax-tipo-almacenamiento-trigger.sql
-@s-06-netmax-tipo-monitor-trigger.sql
-@s-06-netmax-tipo-procesador-trigger.sql
-@s-06-netmax-tipo-tarjeta-video-trigger.sql
+@06-triggers/s-06-ilap-sucursal-trigger.sql
+@06-triggers/s-06-ilap-cah-s2-sucursal-taller-trigger.sql
+@06-triggers/s-06-ilap-cah-s2-sucursal-venta-trigger.sql
+@06-triggers/s-06-ilap-laptop-trigger.sql
+@06-triggers/s-06-ilap-laptop-inventario-trigger.sql
+@06-triggers/s-06-ilap-historico-status-laptop-trigger.sql
+@06-triggers/s-06-ilap-cah-s2-servicio-laptop-trigger.sql
+@06-triggers/s-06-netmax-tipo-almacenamiento-trigger.sql
+@06-triggers/s-06-netmax-tipo-monitor-trigger.sql
+@06-triggers/s-06-netmax-tipo-procesador-trigger.sql
+@06-triggers/s-06-netmax-tipo-tarjeta-video-trigger.sql
 
 Prompt ==============================
 Prompt Creando triggers en mamgbd_s1
 Prompt ==============================
 connect ilap_bdd/ilap_bdd@mamgbd_s1
-@s-06-ilap-sucursal-trigger.sql
-@s-06-ilap-mmg-s1-sucursal-taller-trigger.sql
-@s-06-ilap-mmg-s1-sucursal-venta-trigger.sql
-@s-06-ilap-laptop-trigger.sql
-@s-06-ilap-laptop-inventario-trigger.sql
-@s-06-ilap-historico-status-laptop-trigger.sql
-@s-06-ilap-mmg-s1-servicio-laptop-trigger.sql
-@s-06-netmax-tipo-almacenamiento-trigger.sql
-@s-06-netmax-tipo-monitor-trigger.sql
-@s-06-netmax-tipo-procesador-trigger.sql
-@s-06-netmax-tipo-tarjeta-video-trigger.sql
+@06-triggers/s-06-ilap-sucursal-trigger.sql
+@06-triggers/s-06-ilap-mmg-s1-sucursal-taller-trigger.sql
+@06-triggers/s-06-ilap-mmg-s1-sucursal-venta-trigger.sql
+@06-triggers/s-06-ilap-laptop-trigger.sql
+@06-triggers/s-06-ilap-laptop-inventario-trigger.sql
+@06-triggers/s-06-ilap-historico-status-laptop-trigger.sql
+@06-triggers/s-06-ilap-mmg-s1-servicio-laptop-trigger.sql
+@06-triggers/s-06-netmax-tipo-almacenamiento-trigger.sql
+@06-triggers/s-06-netmax-tipo-monitor-trigger.sql
+@06-triggers/s-06-netmax-tipo-procesador-trigger.sql
+@06-triggers/s-06-netmax-tipo-tarjeta-video-trigger.sql
 
 Prompt ==============================
 Prompt Creando triggers en mamgbd_s2
 Prompt ==============================
 connect ilap_bdd/ilap_bdd@mamgbd_s2
-@s-06-ilap-sucursal-trigger.sql
-@s-06-ilap-mmg-s2-sucursal-taller-trigger.sql
-@s-06-ilap-mmg-s2-sucursal-venta-trigger.sql
-@s-06-ilap-mmg-s2-laptop-trigger.sql
-@s-06-ilap-laptop-inventario-trigger.sql
-@s-06-ilap-historico-status-laptop-trigger.sql
-@s-06-ilap-mmg-s2-servicio-laptop-trigger.sql
-@s-06-netmax-tipo-almacenamiento-trigger.sql
-@s-06-netmax-tipo-monitor-trigger.sql
-@s-06-netmax-tipo-procesador-trigger.sql
-@s-06-netmax-tipo-tarjeta-video-trigger.sql
+@06-triggers/s-06-ilap-sucursal-trigger.sql
+@06-triggers/s-06-ilap-mmg-s2-sucursal-taller-trigger.sql
+@06-triggers/s-06-ilap-mmg-s2-sucursal-venta-trigger.sql
+@06-triggers/s-06-ilap-mmg-s2-laptop-trigger.sql
+@06-triggers/s-06-ilap-laptop-inventario-trigger.sql
+@06-triggers/s-06-ilap-historico-status-laptop-trigger.sql
+@06-triggers/s-06-ilap-mmg-s2-servicio-laptop-trigger.sql
+@06-triggers/s-06-netmax-tipo-almacenamiento-trigger.sql
+@06-triggers/s-06-netmax-tipo-monitor-trigger.sql
+@06-triggers/s-06-netmax-tipo-procesador-trigger.sql
+@06-triggers/s-06-netmax-tipo-tarjeta-video-trigger.sql
 
 Prompt Listo!
